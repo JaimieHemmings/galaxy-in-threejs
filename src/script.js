817402c663b26d1da538f74a1e9dc121bac3f4c4
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-05 11:23:17 +0000

    chore: gui updates and fixes

--- src/script.js
+++ src/script.js
@@ -116,12 +116,12 @@ generateGalaxy()
  * @type {GUI}
  * @private
  */
-gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(1).max(20).step(0.1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(1).max(20).step(1).onFinishChange(generateGalaxy)
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(1).step(0.1).onFinishChange(generateGalaxy)
 gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
 gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
 gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
